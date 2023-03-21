09e29dced349290453727b5aae18f3f3df1ac6c0
jamesk10118 <jamesk10118@gmail.com>
2023-03-21 14:53:43 -0400

    Fixed register, fixed description

--- bugme-frontend/src/app/register/register.component.ts
+++ bugme-frontend/src/app/register/register.component.ts
@@ -6,41 +6,40 @@ import { AppComponent } from '../app.component';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
-
-  constructor(
-    private appComponent: AppComponent
-  ) {
-  }
+  constructor(private appComponent: AppComponent) {}
 
   success: boolean = false;
   errors: Array<any> = [];
 
   usernameCtrl = new FormControl(null, Validators.required);
-  emailCtrl = new FormControl(null, [Validators.required, Validators.email]);
-  passwdCtrl = new FormControl(null, [Validators.pattern(/(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}/), Validators.required]);
-  registerGroup: FormGroup = new FormGroup({ email: this.emailCtrl, passwd: this.passwdCtrl });
+  passwdCtrl = new FormControl(null, [
+    Validators.pattern(/(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}/),
+    Validators.required,
+  ]);
+  registerGroup: FormGroup = new FormGroup({ passwd: this.passwdCtrl });
 
   async attemptCreate() {
     if (this.validateForm()) {
       try {
         let registerInfo = {
-          "email": this.emailCtrl.value,
-          "username": this.usernameCtrl.value,
-          "password": this.passwdCtrl.value,
-          "role": "User"
-        }
-        const response = await axios.post('http://localhost:8080/api/users/all', registerInfo);
+          username: this.usernameCtrl.value,
+          password: this.passwdCtrl.value,
+          role: 'User',
+        };
+        const response = await axios.post(
+          'http://localhost:8080/api/users/all',
+          registerInfo
+        );
 
-        if (response.status = 200) {
+        if ((response.status = 200)) {
           this.showSuccess();
           setTimeout(() => {
             this.routeToLogin();
           }, 3000);
-        }
-        else if (response.status != 200) {
+        } else if (response.status != 200) {
           this.showFailure();
         }
       } catch (error: any) {
@@ -49,15 +48,13 @@ export class RegisterComponent {
           console.log(this.errors);
         }
       }
-    }
-    else{
-      this.errors.push("All inputs are required")
+    } else {
+      this.errors.push('All inputs are required');
     }
   }
 
   validateForm() {
-    if (this.registerGroup.valid)
-      return true;
+    if (this.registerGroup.valid) return true;
     return false;
   }
 
@@ -66,11 +63,10 @@ export class RegisterComponent {
   }
 
   showFailure() {
-    this.errors.push("Unsuccessfull registration attempt, please try again.");
+    this.errors.push('Unsuccessfull registration attempt, please try again.');
   }
 
   routeToLogin() {
-    this.appComponent.navigate("");
+    this.appComponent.navigate('');
   }
-
 }
